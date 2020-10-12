5b1d08a3b6d01d7c33e438567b22782aefa93cb8
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2020-10-12 21:26:23 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,18 @@
 import socket
 from clientThread import ClientThread
-
 HOST = '127.0.0.1'
-PORT = 9999
+PORT = 65435
 client = []
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(4)
-    print('Server is listening...')
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            try:
-                print('Connected by', addr)
-                client.append(ClientThread(conn, addr, client))
-                client[-1].start()
-            except Exception as e:
-                print(e)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(4)
+print('Server is listening...')
+while True:
+    conn, addr = s.accept() 
+    try:
+        print('Connected by', addr)
+        client.append(ClientThread(conn, addr, client))
+        client[-1].start()
+    except Exception as e:
+        print(e)
+conn.close()
