5af5162dfebeb6b17d49f5e5171baeab03a52c2d
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2020-10-18 20:33:07 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,55 @@
+import threading
 import socket
-from clientThread import ClientThread
-HOST = '127.0.0.1'
-PORT = 65435
-client = []
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(4)
-print('Server is listening...')
-while True:
-    conn, addr = s.accept() 
-    try:
-        print('Connected by', addr)
-        client.append(ClientThread(conn, addr, client))
-        client[-1].start()
-    except Exception as e:
-        print(e)
-conn.close()
+#from clientThread import ClientThread
+host = '127.0.0.1'
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nickname[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        
+        print(f'nickname of use client is {nickname}!')
+        broadcast(f'{nickname} joined the chart!'.encode('ascii'))
+        client.send('connect to the server'.encode('ascii'))
+        
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+        
+print("server is listening")
+receive()
+        
+        
+        
+        
+
+
